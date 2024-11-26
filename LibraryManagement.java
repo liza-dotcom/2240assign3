959985149ae432c7c7d209a8896df06447c6a01b
liza-dotcom <lizachoudhry19@gmail.com>
2024-11-26 18:00:28 -0500

    Converted Transaction to Singleton
    Converted the Transaction class to singleton and modified the LibraryManagement class to call methods using the single instance of Transaction class rather than having static method calls.

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -1,3 +1,4 @@
+package classes;
 import java.util.Scanner;
 
 public class LibraryManagement {
@@ -5,6 +6,7 @@ public class LibraryManagement {
 
     public static void main(String[] args) {
         new LibraryManagement().run();
+        Transaction transaction= Transaction.getTransaction();
     }
 
     private void run() {
@@ -76,7 +78,7 @@ public class LibraryManagement {
                     Book book = library.findBookById(bookId);
 
                     if (member != null && book != null) {
-                    	Transaction.borrowBook(book, member);
+                    	Transaction.getTransaction().borrowBook(book, member);
                     } else {
                         System.out.println("Invalid member or book ID.");
                     }
@@ -94,7 +96,7 @@ public class LibraryManagement {
                     book = library.findBookById(bookId);
 
                     if (member != null && book != null) {
-                    	Transaction.returnBook(book, member);
+                    	Transaction.getTransaction().returnBook(book, member);
                     } else {
                         System.out.println("Invalid member or book ID.");
                     }
@@ -116,7 +118,7 @@ public class LibraryManagement {
                     }
                     break;
                 case 6:
-                	Transaction.displayTransactionHistory();
+                	Transaction.getTransaction().displayTransactionHistory();
                     break;
                 case 7:
                     System.out.println("Exiting. Good Bye..");
--- Transaction.java
+++ Transaction.java
@@ -1,10 +1,15 @@
+package classes;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
 
+	// making the private constructor to convert to singleton
+	private Transaction () {}
+	// creating an instance variable to access all the methods of the singleton class
+	private static Transaction instance;
     // Perform the borrowing of a book
-    public static boolean borrowBook(Book book, Member member) {
+    public  boolean borrowBook(Book book, Member member) {
         if (book.isAvailable()) {
             book.borrowBook();
             member.borrowBook(book); 
@@ -18,7 +23,7 @@ public class Transaction {
     }
 
     // Perform the returning of a book
-    public static void returnBook(Book book, Member member) {
+    public  void returnBook(Book book, Member member) {
         if (member.getBorrowedBooks().contains(book)) {
             member.returnBook(book);
             book.returnBook();
@@ -30,8 +35,16 @@ public class Transaction {
     }
 
     // Get the current date and time in a readable format
-    private static String getCurrentDateTime() {
+    private  String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
+    }
+    public static Transaction getTransaction()
+    {
+    	if (instance==null)
+    	{
+    		instance= new Transaction(); // Create a new instance if there was no single/only instance initially.
+    	}
+    	return instance;
     }
 }
