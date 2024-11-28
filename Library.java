6101fc031d5c06ca51f4ba9bfafab809f2c60e77
liza-dotcom <lizachoudhry19@gmail.com>
2024-11-27 19:55:51 -0500

    Updated the addBook () and addMember() methods to check for duplicate IDs.

--- Library.java
+++ Library.java
@@ -5,14 +5,35 @@ public class Library {
     private List<Member> members = new ArrayList<Member>();
     private List<Book> books = new ArrayList<Book>();
 
-    // Add a new member to the library
-    public void addMember(Member member) {
-        members.add(member);
+ // Add a new member to the library
+    public boolean addMember(Member member) {
+       if(findMemberById(member.getId())!=null)// check if the member with same ID already exists.
+       
+       {  //Display a message if another member has a matching Id.
+    	  System.out.println("Cannot add the member as the provided ID already exists in the system");
+    	  return false;
+       }
+       else 
+       {   //add member if no member has a matching id.
+    	   members.add(member);
+    	  return true;
+       }
+      
     }
     
     // Add a new book to the library
-    public void addBook(Book book) {
-        books.add(book);
+    public boolean addBook(Book book) {
+        if(findBookById(book.getId())!=null)
+        {   //Display an appropriate message
+        	System.out.println("Cannot add the book as there is already a book with same ID!");
+        	return false;
+        }
+        else
+        {   //add book if there is no matching id in the system.
+        	books.add(book);
+        	return true;
+        }
+    	
     }
 
     // Find a member by ID
